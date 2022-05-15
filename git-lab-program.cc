6ade3a3c99eeb99b28b766d7984fe441e4a6c6b3
Aaron Reynolds <ar796620@ohio.edu>
2022-05-15 00:08:10 -0400

    Git Lab C++ Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Aaron Reynolds
+ *        Date: 5/14/2022
+ * Description: basic c++ program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
